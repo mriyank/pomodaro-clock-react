cfac26f09a52fbb65f483ea5228a909a5febc6ab
mriyank <mriyanksingh@gmail.com>
2020-11-30 21:25:00 +0530

    added break & session buttons js

--- src/components/App.js
+++ src/components/App.js
@@ -13,17 +13,61 @@ class App extends React.Component {
       sessionLength: 25,
       timerMinute: 25,
     };
+    this.onIncreaseBreakLength = this.onIncreaseBreakLength.bind(this);
+    this.onDecreaseBreakLength = this.onDecreaseBreakLength.bind(this);
+    this.onIncreaseSessionLength = this.onIncreaseSessionLength.bind(this);
+    this.onDecreaseSessionLength = this.onDecreaseSessionLength.bind(this);
   }
+
+  onIncreaseBreakLength() {
+    this.setState((prevState) => {
+      return {
+        breakLength: prevState.breakLength + 1,
+      };
+    });
+  }
+
+  onDecreaseBreakLength() {
+    this.setState((prevState) => {
+      return {
+        breakLength: prevState.breakLength - 1,
+      };
+    });
+  }
+
+  onIncreaseSessionLength() {
+    this.setState((prevState) => {
+      return {
+        sessionLength: prevState.sessionLength + 1,
+      };
+    });
+  }
+
+  onDecreaseSessionLength() {
+    this.setState((prevState) => {
+      return {
+        sessionLength: prevState.sessionLength - 1,
+      };
+    });
+  }
+
   render() {
     return (
       <main>
         <h2>Pomodoro Clock</h2>
         <section className="internal-length-container">
-        <BreakInterval breakInterval={this.state.breakLength} />
-        <SessionLength sessionLength={this.state.sessionLength} />
+          <BreakInterval
+            breakInterval={this.state.breakLength}
+            increaseBreak={this.onIncreaseBreakLength}
+            decreaseBreak={this.onDecreaseBreakLength}
+          />
+          <SessionLength
+            sessionLength={this.state.sessionLength}
+            increaseSession={this.onIncreaseSessionLength}
+            decreaseSession={this.onDecreaseSessionLength}
+          />
         </section>
         <Timer timerMinute={this.state.timerMinute} />
-        
       </main>
     );
   }
--- src/components/SessionLength.js
+++ src/components/SessionLength.js
@@ -1,13 +1,27 @@
 import React from "react";
 
 function SessionLength(props) {
+  function increaseSession() {
+    if (props.sessionLength === 60) {
+      return;
+    }
+    props.increaseSession();
+  }
+
+  function decreaseSession() {
+    if (props.sessionLength === 1) {
+      return;
+    }
+    props.decreaseSession();
+  }
+
   return (
     <section>
-      <h4 className="">Session Length</h4>
+      <h4>Session Length</h4>
       <section className="interval-container">
-        <button>Down</button>
+        <button onClick={decreaseSession}>Down</button>
         <p className="interval-length">{props.sessionLength}</p>
-        <button>Up</button>
+        <button onClick={increaseSession}>Up</button>
       </section>
     </section>
   );
